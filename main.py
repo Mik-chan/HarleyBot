151c760b80e11cd90938050b30ccac3d1d03e924
wtlgo <georgeendo0@gmail.com>
2020-01-17 18:49:04 +0300

    pep8 formatting

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from HarleyBot import HarleyBot
 
 def main():
     config = {
-        "api_key": "3d7683f13e30172b7f5ebe5448cef62d992975d1f7e6915bb0ef71d9087baadfa3538c76ef8e5a6ba1bed",
+        "api_key":
+            "3d7683f13e30172b7f5ebe5448ce"
+            "f62d992975d1f7e6915bb0ef71d9"
+            "087baadfa3538c76ef8e5a6ba1bed",
         "group_id": "61782600",
         "creator": 99806575
     }
